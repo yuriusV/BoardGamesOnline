8741b68d44c4f1900128b6d27432c7ddba633646
Yurius <yuravashchenko15@gmail.com>
2016-11-12 23:32:55 +0200

    make structure of files, create project

---
+++ Source/BoardGameOnline/AppStart/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LightInject;
+
+namespace AppStart
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main( )
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+        }
+    }
+}
